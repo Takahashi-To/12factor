d3ea1582b17c7be456a37c8895e0147122c36183
to.takahashi <to.takahashi@os.infocom.co.jp>
2023-09-26 11:28:24 +0900

    4章終了

--- TFApp_before/Pages/Register.cshtml.cs
+++ TFApp_before/Pages/Register.cshtml.cs
@@ -2,11 +2,19 @@
 
 public class RegisterModel : PageModel
 {
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly ILogger<RegisterModel> _logger;
     private readonly TFAppContext _context;
 
-    public RegisterModel(TFAppContext context)
+    public RegisterModel(
+        TFAppContext context,
+        IHttpContextAccessor httpContextAccessor,
+        ILogger<RegisterModel> logger
+    )
     {
         _context = context;
+        _httpContextAccessor = httpContextAccessor;
+        _logger = logger;
     }
 
     [BindProperty]
@@ -16,7 +24,7 @@ public class RegisterModel : PageModel
 
     public async Task<IActionResult> OnGetAsync()
     {
-        var session = HttpContext.Session;
+        var session = _httpContextAccessor.HttpContext.Session;
         var key = session.GetString(SessionKey);
 
         if (_context.User != null)
@@ -48,16 +56,20 @@ public class RegisterModel : PageModel
             _context.User.Add(user);
             await _context.SaveChangesAsync();
 
-            System.IO.File.AppendAllText(@"./log.txt", $"{DateTime.Now:F}: UserIdをDBに保存しました\n");
+            // System.IO.File.AppendAllText(@"./log.txt", $"{DateTime.Now:F}: UserIdをDBに保存しました\n");
+            _logger.LogInformation($"{DateTime.Now:F}: UserId を DB に保存しました");
 
             // セッションに保存
-            HttpContext.Session.SetString(SessionKey, user.UserId);
+            // HttpContext.Session.SetString(SessionKey, user.UserId);
+            _httpContextAccessor.HttpContext.Session.SetString(SessionKey, user.UserId);
 
-            System.IO.File.AppendAllText(@"./log.txt", $"{DateTime.Now:F}: UserIdをセッションに保存しました\n");
+            // System.IO.File.AppendAllText(@"./log.txt", $"{DateTime.Now:F}: UserIdをセッションに保存しました\n");
+            _logger.LogInformation($"{DateTime.Now:F}: UserId をセッションに保存しました");
         }
         else
         {
-            System.IO.File.AppendAllText(@"./log.txt", $"{DateTime.Now:F}: DBへの保存が失敗しました\n");
+            // System.IO.File.AppendAllText(@"./log.txt", $"{DateTime.Now:F}: DBへの保存が失敗しました\n");
+            _logger.LogError($"{DateTime.Now:F}: DB への保存が失敗しました");
         }
 
         return RedirectToPage("./Index");
